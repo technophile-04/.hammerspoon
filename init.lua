4af954b49e784713f53a79adfe53c780bc935182
Shiv Bhonde <shivbhonde04@gmail.com>
2025-11-10 13:09:03 +0100

    add shortcut for gpt and telegram

--- init.lua
+++ init.lua
@@ -36,3 +36,21 @@ hs.hotkey.bind(hyper, "O", function()
     hs.application.launchOrFocus("Obsidian")
   end
 end)
+
+hs.hotkey.bind(hyper, "T", function()
+  local app = hs.application.get("com.tdesktop.Telegram")
+  if app then
+    app:activate()
+  else
+    hs.application.launchOrFocus("Telegram")
+  end
+end)
+
+hs.hotkey.bind(hyper, "G", function()
+  local app = hs.application.get("com.openai.chat")
+  if app then
+    app:activate()
+  else
+    hs.application.launchOrFocus("ChatGPT")
+  end
+end)
